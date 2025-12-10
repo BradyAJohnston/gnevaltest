ba6797960f2dadf8d613dbbadde5ec2ff393dc1a
Brady Johnston <brady.johnston@me.com>
2025-12-10 11:37:54 +0000

    cleanup test script

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import bpy
 import time
 
-FILEPATH = "basic_particle_simulation.blend"
-N_FRAMES = 100
-DENSITY = 1_000
-
 def _bake_print_time(obj: bpy.types.Object):
     obj.select_set(True)
     start = time.time()
@@ -12,17 +8,25 @@ def _bake_print_time(obj: bpy.types.Object):
     end = time.time()
     print(end - start)
 
-
-def main():
-    bpy.ops.wm.open_mainfile(filepath=FILEPATH)
-    bpy.context.scene.frame_end = N_FRAMES
+def test_particle(n_frames: int = 100, density: int = 1_000):
+    bpy.ops.wm.open_mainfile(filepath="basic_particle_simulation.blend")
+    bpy.context.scene.frame_end = n_frames
     obj = bpy.data.objects["Particles"]
-    obj.modifiers["GeometryNodes"]["Input_3"] = DENSITY
+    obj.modifiers["GeometryNodes"]["Input_3"] = density
     _bake_print_time(obj)
-    
+
+def test_raycast(cubes: int = 10_000, points: int = 10_000):
     bpy.ops.wm.open_mainfile(filepath="raycasting.blend")
     obj = bpy.data.objects["Cube"]
+    tree = obj.modifiers["GeometryNodes"].node_group
+    tree.nodes["Points.001"].inputs["Count"].default_value = cubes
+    tree.nodes["Points"].inputs["Count"].default_value = points
+    
     _bake_print_time(obj)
+
+def main():
+    test_particle()
+    test_raycast()
 
 
 if __name__ == "__main__":
