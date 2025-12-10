dc5586e19e4eac592ba3f200e735cbcfb3c2cf57
Brady Johnston <brady.johnston@me.com>
2025-12-10 11:25:56 +0000

    add raycast test

--- main.py
+++ main.py
@@ -2,19 +2,27 @@ import bpy
 import time
 
 FILEPATH = "basic_particle_simulation.blend"
-N_FRAMES = 200
+N_FRAMES = 100
 DENSITY = 1_000
+
+def _bake_print_time(obj: bpy.types.Object):
+    obj.select_set(True)
+    start = time.time()
+    bpy.ops.object.simulation_nodes_cache_bake(selected=True)
+    end = time.time()
+    print(end - start)
+
 
 def main():
     bpy.ops.wm.open_mainfile(filepath=FILEPATH)
     bpy.context.scene.frame_end = N_FRAMES
     obj = bpy.data.objects["Particles"]
-    obj.select_set(True)
     obj.modifiers["GeometryNodes"]["Input_3"] = DENSITY
-    start = time.time()
-    bpy.ops.object.simulation_nodes_cache_bake(selected=True)
-    end = time.time()
-    print(end - start)
+    _bake_print_time(obj)
+    
+    bpy.ops.wm.open_mainfile(filepath="raycasting.blend")
+    obj = bpy.data.objects["Cube"]
+    _bake_print_time(obj)
 
 
 if __name__ == "__main__":
